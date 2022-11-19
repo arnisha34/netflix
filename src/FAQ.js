aa5f4bbdc7700707b30457646cb5689f38731381
ArNisha Henderson-Jones <arnishahenderson-jones@ArNishas-iMac.local>
2022-11-19 00:38:50 -0800

    working on the FAQ toggle

--- src/FAQ.js
+++ src/FAQ.js
@@ -1,10 +1,42 @@
 import { Col, Row } from "react-bootstrap"
+import { faqData } from "./FAQData"
+import {GrAdd} from "react-icons/gr"
+import { useState } from "react"
 
 const FAQ = () => {
+
+  const [active, setActive] = useState(false)
+  const [toggle, setToggle] = useState([]);
+  
+  const selected = faqData.map(item => item.id);
+
+  const handleClick = (id) => {
+    setActive(!active);
+    setToggle(selected.find(item => item === id));
+  }
+
   return(
-    <Row className="sign-in-card">
+    <Row className="sign-in-card faq">
       <Col>
-        <h2 className="card-title-h2 text-center">Frequently Asked Questions</h2>
+          <Row className="mb-5">
+            <Col>
+                <h2 className="card-title-h2 text-center">Frequently Asked Questions</h2>
+            </Col>
+          </Row>
+          <Row className="faq-container mx-auto">
+            <Col>
+              <ul className="faq-list">
+                {faqData.map((item, id) => {
+                  return(
+                  <li key={item.id} className="faq-list-item">
+                    <button className={`faq-question border-0 text-start position-relative ${active === true ? "active" : ""}`} onClick={() => handleClick(id)}>{item.question} <GrAdd className="plus-icon position-absolute"/></button>
+                    <div className={`faq-answer ${toggle === id && !active ? "open" : "closed"}`}>{item.answer}</div>
+                  </li>
+                  )})}
+
+              </ul>
+            </Col>
+          </Row>
       </Col>
     </Row>
   )
--- src/Hero.js
+++ src/Hero.js
@@ -1,21 +1,22 @@
 import { Row, Col, InputGroup, Form, Button } from "react-bootstrap";
+import { HiChevronRight } from 'react-icons/hi'
 
 const Hero = () => {
   return (
     <>
       <Row className="hero-container justify-content-center align-content-center">  
-        <Col className="hero-content text-center">
+        <Col className="hero-content text-center" lg={5}>
           <h1 className="top-header">Unlimited movies, TV shows, and more.</h1>
           <h3 className="mt-3 mb-5">Watch anywhere. Cancel anytime.</h3>
           <h5 className="mb-4">Ready to watch? Enter your email to create or restart your membership.</h5>
-          <InputGroup className="mb-3">
+          <InputGroup className="mb-3 signup">
             <Form.Control
               placeholder="Email address"
               aria-label="Email address"
               aria-describedby="basic-addon2"
             />
-            <Button variant="outline-secondary" id="button-addon2">
-              Get Started
+            <Button variant="outline-secondary" className="redButton">
+              Get Started <HiChevronRight />
             </Button>
           </InputGroup>
         </Col>
